360f271b75b6037f25b0335fda0c875ce062adab
Khaja-a173 <shaik.khaja.moinuddin173@gmail.com>
2025-08-24 22:20:59 +1000

    Updated vitest.ui.config.ts

---
+++ vitest.ui.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    name: 'ui',
+    reporters: ['dot'],
+    hookTimeout: 30_000,
+    testTimeout: 30_000,
+    include: ['tests/ui/**/*.spec.ts', 'tests/ui/**/*.spec.tsx'],
+    environment: 'jsdom',
+    environmentOptions: { jsdom: { url: 'http://localhost' } },
+    setupFiles: ['tests/loadEnv.ts', 'tests/ui/setupJSDOM.ts'],
+    isolate: true,
+    clearMocks: true,
+  },
+});
