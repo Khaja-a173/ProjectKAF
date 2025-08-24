f8ed6e7ac05650ed0c97762fc40a53490cdc2e4d
Khaja-a173 <shaik.khaja.moinuddin173@gmail.com>
2025-08-24 20:44:30 +1000

    Updated vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,27 +1,27 @@
-import { defineConfig } from 'vitest/config';
+// vitest.config.ts
+import { defineWorkspace } from 'vitest/config';
 
-export default defineConfig({
-  test: { 
-    reporters: ['dot'], 
-    hookTimeout: 30000, 
-    testTimeout: 30000 
+export default defineWorkspace([
+  {
+    test: {
+      name: 'api',
+      reporters: ['dot'],
+      hookTimeout: 30_000,
+      testTimeout: 30_000,
+      include: ['tests/table-session.spec.ts', 'tests/orders.spec.ts'],
+      environment: 'node',
+      setupFiles: ['tests/loadEnv.ts', 'tests/setupServer.ts'],
+    },
   },
-  projects: [
-    {
-      test: {
-        name: 'api',
-        include: ['tests/table-session.spec.ts', 'tests/orders.spec.ts'],
-        environment: 'node',
-        setupFiles: ['tests/loadEnv.ts', 'tests/setupServer.ts'],
-      },
+  {
+    test: {
+      name: 'ui',
+      reporters: ['dot'],
+      hookTimeout: 30_000,
+      testTimeout: 30_000,
+      include: ['tests/ui/**/*.spec.ts', 'tests/ui/**/*.spec.tsx'],
+      environment: 'jsdom',
+      setupFiles: ['tests/loadEnv.ts'],
     },
-    {
-      test: {
-        name: 'ui',
-        include: ['tests/ui/**/*.spec.ts', 'tests/ui/**/*.spec.tsx'],
-        environment: 'jsdom',
-        setupFiles: ['tests/loadEnv.ts'],
-      },
-    },
-  ],
-});
+  },
+]);
