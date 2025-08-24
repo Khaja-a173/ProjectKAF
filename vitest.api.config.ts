21dcb64fa780c38d005a2c74c1db35f7ee0504c8
Khaja-a173 <shaik.khaja.moinuddin173@gmail.com>
2025-08-24 22:20:48 +1000

    Updated vitest.api.config.ts

---
+++ vitest.api.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    name: 'api',
+    reporters: ['dot'],
+    hookTimeout: 30_000,
+    testTimeout: 30_000,
+    include: ['tests/table-session.spec.ts', 'tests/orders.spec.ts'],
+    environment: 'node',
+    setupFiles: ['tests/loadEnv.ts', 'tests/setupServer.ts'],
+    isolate: true,
+    clearMocks: true,
+  },
+});
